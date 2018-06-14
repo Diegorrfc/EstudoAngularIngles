fb1339a2b8fa40c169ec60250867ae385921f6b7
Diego <diego.rrfc@gmail.com>
2018-06-14 01:24:23 -0300

    primeiro

---
+++ app1/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { TopoComponent } from './Topo/topo.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopoComponent,
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
